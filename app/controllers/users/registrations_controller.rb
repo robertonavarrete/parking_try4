f761eb5f98f5e8b3fc965f6644cded2dcf24d4a7
klavikula <robertonavarrete@outlook.com>
2020-03-09 13:48:18 -0300

    adding yarns js and navar for application.html.erb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -48,7 +48,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:identification_type, :identification_number, :name, :last_name])
-  end])
   end
 
   # The path used after sign up.
