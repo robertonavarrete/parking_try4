0a3ad0c077e7703214dd57c31387f20afe54c896
klavikula <robertonavarrete@outlook.com>
2020-03-20 11:17:27 -0300

    trying upload to heroku v1 fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get 'billings/pre_pay', to:'billings#pre_pay', as: 'pre_pay_billings'
   get 'billings/execute', to:'billings#execute', as:'execute_billings'
   
-  root 'bookings#new'
+  root 'tickets#new'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
